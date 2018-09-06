d215df354ce4d06b1108fb5087511c887c94fb0d
inakleinbottle <41870650+inakleinbottle@users.noreply.github.com>
2018-09-06 08:09:12 +0100

    Added setup script

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+
+
+setup(name='simplemmerge',
+      author='inakleinbottle',
+      author_email='admin@inakleinbottle.com',
+      version='0.1.0',
+      license='GPL-3.0',
+      long_description=open('README.md').read(),
+      description='Simple mail merge program that formats a template file with entries from CSV.',
+      py_modules=['mmerge'],
+      entry_points={
+        'console_scripts' : ['mmerge = mmerge:main']
+      }
+      )
+      
+      
+      
